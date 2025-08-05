ec903e2a2e856150f0ec3c6452d4241bf725ab02
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-05 12:02:54 -0300

    Feat: Classe de ConsoleUI criada, por hora, apenas exibe mensagens.

---
+++ Scripts/Miscelania.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public static class ConsoleUI
+    {
+        public static void Exibir(Mensagem mensagem)
+        {
+            string prefixo = $"{mensagem.Tipo.ToString().ToUpper()}: ";
+            Console.WriteLine($"{prefixo} {mensagem.Conteudo}");
+
+            if (mensagem.EsperarTecla)
+            {
+                Console.WriteLine("(Pressione qualquer tecla para continuar)");
+                Console.ReadKey();
+            }
+            if (mensagem.LimparConsole)
+            {
+                Console.Clear();
+            }
+        }
+    }
+}
