759c5d7c61b8f32b4fdda07b262d1304d2b175a3
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-05 11:33:57 -0300

    Feat: Criação dos tipos e dos principais atributos de uma mensagem.

---
+++ Scripts/Mensagem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Miscelania
+{
+    public enum TipoMensagem
+    {
+        Alerta,
+        Erro,
+        Informacao,
+        Sucesso
+    }
+    public class Mensagem
+    {
+        public string Conteudo { get; }
+        public TipoMensagem Tipo { get; }
+        public bool EsperarTecla { get; }
+        public bool LimparConsole { get; }
+
+        public Mensagem(string conteudo, TipoMensagem tipo, bool esperarTecla, bool limparConsole)
+        {
+            Conteudo = conteudo;
+            Tipo = tipo;
+            EsperarTecla = esperarTecla;
+            LimparConsole = limparConsole;
+        }
+    }
+}
