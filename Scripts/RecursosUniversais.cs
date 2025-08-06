1b0e454c2df5550602ccc8711ccdd318331a9990
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-06 16:30:59 -0300

    Fix: Alteração que promove a interface do usuário, as quais permitem visualizar as mensagens de erro.

--- Scripts/RecursosUniversais.cs
+++ Scripts/RecursosUniversais.cs
@@ -5,14 +5,10 @@ namespace UniversalResources
     public static class RecursosUniversais
     {
         public static string PedirEntrada(string msg)
-        {
-            while (true)
-            {
+        { 
                 Console.Write(msg);
                 string? input = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(input)) continue;
-                else return input;
-            }
+                return input!;
         }
     }
 }
