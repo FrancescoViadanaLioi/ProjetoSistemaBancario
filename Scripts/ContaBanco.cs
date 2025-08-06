f99d44201ba4aa75a2f2a831a4ffdbf0fa11517d
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-06 16:35:11 -0300

    Fix: Ao invés de exceções, usei mensagens de erro para melhor interface de usuário.

--- Scripts/ContaBanco.cs
+++ Scripts/ContaBanco.cs
@@ -1,45 +1,48 @@
-﻿using System;
+﻿using Miscelania;
+using System;
 using Validations;
-
 namespace ContaBanco.Info
 {
     public class DadosBancarios
     {
-        private string _nome;
-        private string _pin;
-        private double _saldo;
-
+        private string _nome = "";
+        private string _pin = "";
+        private double _saldo = 0;
         public string Nome => _nome;
         public string PIN => _pin;
         public double Saldo => _saldo;
-
         public DadosBancarios(string nome, string pin, double saldo)
         {
             if (!ValidacoesDeDados.ValidarNome(nome))
             {
-                throw new ArgumentException("O nome é inválido.");
+                ConsoleUI.ExibirErro("O nome é inválido.");
+                return;
             }
             if (!ValidacoesDeDados.ValidarPIN(pin))
             {
-                throw new ArgumentException("O pin é inválido.");
+                ConsoleUI.ExibirErro("O pin é inválido.");
+                return;
             }
             if (!ValidacoesDeDados.ValidarSaldoPositivo(saldo, out double saldoValidado))
             {
-                throw new ArgumentException("O saldo é inválido. Se você digitou números, verifique se são maiores que zero.");
+                ConsoleUI.ExibirErro("O saldo é inválido. Se você digitou números, verifique se são maiores que zero.");
+                return;
             }
-                _nome = nome;
-                _pin = pin;
-                _saldo = saldoValidado;
+            _nome = nome;
+            _pin = pin;
+            _saldo = saldoValidado;
         }
         public void Sacar(double saque)
         {
             if (!ValidacoesDeDados.ValidarSaque(saque, out double saqueValidado))
             {
-                throw new ArgumentException("Verifique se o saque é um número real maior do que 0.");
+                ConsoleUI.ExibirErro("Verifique se o saque é um número real maior do que 0.");
+                return;
             }
             if (saque > _saldo)
             {
-                throw new ArgumentException("O saque é maior do que o saldo. Operação cancelada.");
+                ConsoleUI.ExibirErro("O saque não pode ser maior do que o saldo.");
+                return;
             }
             _saldo -= saque;
         }
@@ -47,7 +50,8 @@ namespace ContaBanco.Info
         {
             if (!ValidacoesDeDados.ValidarDeposito(deposito, out double depositoValidado))
             {
-                throw new ArgumentException("O depósito deve ser um número positivo.");
+                ConsoleUI.ExibirErro("O depósito deve ser um número positivo.");
+                return;
             }
             _saldo += deposito;
         }
