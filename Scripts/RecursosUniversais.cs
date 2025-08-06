11b43eeeea08706156dd3b8cbb50d2d5702a0d36
FrancescoViadanaLioi <francescogiovannilioi@gmail.com>
2025-08-06 11:28:35 -0300

    Feat: Classe de recursos universais, a qual até o momento apenas possui um método de leitura de entrada.

---
+++ Scripts/RecursosUniversais.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace UniversalResources
+{
+    public static class RecursosUniversais
+    {
+        public static string PedirEntrada(string msg)
+        {
+            while (true)
+            {
+                Console.Write(msg);
+                string? input = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input)) continue;
+                else return input;
+            }
+        }
+    }
+}
